f56966d56921d2e7e7e1bcc8ce7addfda3ade214
Swiftmatt <15731984+Swiftmatt@users.noreply.github.com>
2021-01-11 15:48:16 -0500

    Add better tracking and unified support for days of the week

--- src/lib/Date.ts
+++ src/lib/Date.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+type DayOfTheWeekNumber = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+
 export enum Day {
     Sunday = 'Sunday',
     Monday = 'Monday',
@@ -6,4 +9,19 @@ export enum Day {
     Thursday = 'Thursday',
     Friday = 'Friday',
     Saturday = 'Saturday',
+}
+
+export const dayOfTheWeekNumberMap: Record<DayOfTheWeekNumber, Day> = {
+    0: Day.Sunday,
+    1: Day.Monday,
+    2: Day.Tuesday,
+    3: Day.Wednesday,
+    4: Day.Thursday,
+    5: Day.Friday,
+    6: Day.Saturday,
+};
+
+export function getDayOfTheWeekFromDate(date: Date): Day {
+    const dayOfTheWeekNumber = date.getDay() as DayOfTheWeekNumber;
+    return dayOfTheWeekNumberMap[dayOfTheWeekNumber];
 }
