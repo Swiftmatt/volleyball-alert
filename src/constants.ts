b54b66d502ff6fd6650bdef489861b113fad7edd
Swiftmatt <15731984+Swiftmatt@users.noreply.github.com>
2021-02-19 03:12:12 -0500

    Extract message creation to its own file and utilize handlebars.js to create the message

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,13 @@
+/**
+ * WARNING - If this file is moved from `./src`, the app will break.
+ * See usages of `__dirname` below.
+ */
+
 import * as Path from 'path';
 
 
 export const ENV_FILE_NAME = '.env';
 
-export const PROJECT_ROOT_PATH = Path.resolve(__dirname, '../');
+export const SRC_PATH = Path.resolve(__dirname);
+export const PROJECT_ROOT_PATH = Path.resolve(SRC_PATH, '../');
 export const ENV_FILE_PATH = Path.resolve(PROJECT_ROOT_PATH, ENV_FILE_NAME);
---
+++ src/lib/Message/index.ts
@@ -0,0 +1,51 @@
+import * as DateFns from 'date-fns';
+import {promises as Fs} from 'fs';
+import * as Handlebars from 'handlebars';
+import * as Path from 'path';
+
+import { SRC_PATH } from 'src/constants';
+import type { LeagueMatch } from 'src/models/Match';
+
+
+const TEMPLATES_PATH = Path.resolve(SRC_PATH, './templates');
+const DEFAULT_TEMPLATE_FILE_NAME = 'message.hbs';
+
+
+export async function createLeagueMatchMessage(leagueMatch: LeagueMatch): Promise<string> {
+    const path = Path.resolve(TEMPLATES_PATH, DEFAULT_TEMPLATE_FILE_NAME);
+    const templateBuffer = await Fs.readFile(path);
+    const templateFile = templateBuffer.toString();
+
+    const {
+        court,
+        datetime,
+        league,
+        opponentTeam,
+        team,
+    } = leagueMatch;
+
+    const date = DateFns.format(
+        datetime,
+        'MMMM do, yyyy',
+    );
+    const time = DateFns.format(
+        datetime,
+        'h:mm a',
+    );
+    const teamRecord = team.record.join('-');
+    const opponentTeamRecord = opponentTeam.record.join('-');
+
+    const template = Handlebars.compile(templateFile);
+    const message = template({
+        court,
+        date,
+        league,
+        opponentTeam,
+        opponentTeamRecord,
+        team,
+        teamRecord,
+        time,
+    });
+
+    return message;
+}
